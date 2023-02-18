864d80aaa2808aafd408250ea32fd372f02bc820
Amir Pourmand <pourmand1376@gmail.com>
2023-02-18 15:31:11 +0330

    add memento pattern

---
+++ 1-memento/history.py
@@ -0,0 +1,9 @@
+class History():
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
